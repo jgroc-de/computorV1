a2904e0804033742f99fc8d1592feca038f5a709
jgroc-de <jgroc@ccmbenchmark.com>
2020-06-26 00:14:30 +0200

    rework lexer

--- ft_test/validity_test.py
+++ ft_test/validity_test.py
@@ -1,24 +1,70 @@
 import os
 
+from src.computor import compute
 
-def execute_test(test):
-    print('test: ' + test[0])
-    out = os.system('python3 computorV1 "{}"'.format(test[0]))
-    if out == test[1] * 256:
-        print('\033[92m' + 'OK' + '\033[0m')
+def assert_equal(test):
+    print('                  ', end='\r')
+    print(*test, end = '')
+    out = compute(test[0])
+    if out == test[1]:
+        print('\033[92m' + ' OK' + '\033[0m', end = '')
     else:
-        print('\033[91m' + 'NOK' + '\033[0m')
+        print(" - result: " + str(out), end = '')
+        print('\033[91m' + ' NOK' + '\033[0m', end = '')
+        print('')
+        exit()
+
+def assert_throw(test):
+    print('                  ', end='\r')
+    print(*test, end = '')
+    try:
+        compute(test[0])
+    except ValueError as error:
+        if test[1] == 'value':
+            print('\033[92m' + 'OK' + '\033[0m', end = '')
+        else:
+            print(error, end = '')
+            print('\033[91m' + 'NOK' + '\033[0m', end = '')
+            print('')
+        return
+    print(error, end = '')
+    print('\033[91m' + 'NOK' + '\033[0m', end = '')
+    print('')
 
 
 def tests():
+    basic_op = [
+        ["1+1", 2],
+        ["1.1+2.2", 3.3],
+        ["0.3+0.3+0.3-0.9", 0],
+        ["2*4", 8],
+        ["2/4", 0.5],
+        ["1/3", 0.33333333],
+        ["0+0", 0],
+        ["0-0", 0],
+        ["2 * 3", 6],
+        ["2 * 3 * 2 - 2", 10],
+        ["2 * 3 + 3 * 2 / 2", 9],
+        ["(2+5) * 2", 14],
+        ["(5) * (2)", 10],
+        ["(3 + 2) * (2 + 3)", 25],
+        ["(3 + 2) / (2 + 3)", 1],
+        ["(3 + 2.0) / (2 + 3)", 1],
+        ["(3.0) / (3.0 + 3)", 0.5],
+        ["((3.0)) / (3.0 + 3)", 0.5],
+        ["((3.0) / (3.0 + 3))", 0.5],
+        ["(3.0 + (3 + 4)) / (3.0 + 2)", 2],
+
+    ]
     equation_errors = [
-        ["", 1],
-        ["lol",  1],
-        ["lol = lol",  1],
-        [" 3 * X + 2 * Y = 0",  1],
-        [" 3 * X = 2 * Y",  1],
-        ["3 * X = 2 * Y ^ 2",  1],
-        [" 3 * X ^ 1 * X ^ 2 = 0",  1],
+        #["1.1.1+2.2", 'value'],
+        ["", 'value'],
+        ["lol",  0],
+        ["lol = lol",  0],
+        [" 3 * X + 2 * Y = 0",  0],
+        [" 3 * X = 2 * Y",  0],
+        ["3 * X = 2 * Y ^ 2",  0],
+        [" 3 * X ^ 1 * X ^ 2 = 0",  0],
     ]
 
     equation_0 = [
@@ -54,18 +100,25 @@ def tests():
         ["-2 + 3 * X ^ 2 + 4 * X = 0",  0],
         ["-2 + 4 * X + 3 * X ^ 2 = 0",  0],
     ]
+    
+    for test in basic_op:
+        assert_equal(test)
+    print('')
 
+    
     for test in equation_errors:
-        execute_test(test)
+        assert_throw(test)
 
     for test in equation_0:
-        execute_test(test)
+        assert_equal(test)
 
     for test in equation_1:
-        execute_test(test)
+        assert_equal(test)
 
     for test in equation_2:
-        execute_test(test)
+        assert_equal(test)
+
+    print('')
 
 
 tests()
--- src/computor.py
+++ src/computor.py
@@ -1,18 +1,29 @@
-from src.computor_src import lexer, parser
+from src.lexer.lexer import Lexer
+from src.parser.parser import Parser
 
 
 def compute(calculus: str) -> float:
     calculus_parts = calculus.split('=')
+    my_lexer = Lexer()
+    my_parser = Parser()
+    result = 0
     if len(calculus_parts) == 1:
-        tokens = lexer.tokenize(calculus)
+        tokens = my_lexer.tokenize(calculus)
         if len(tokens) == 0:
-            raise ValueError('error')
-        result = parser.parse_recursive(tokens)
-
-        return result
+            raise ValueError('lexer error')
+        try:
+            result = my_parser.parse_recursive(tokens)
+        except ValueError:
+            raise ValueError('parser error')
     elif len(calculus_parts) == 2:
         if len(calculus_parts[0]) == 0 or len(calculus_parts[1]) == 0:
             raise SyntaxError('one part of the equation is empty')
         pass
     else:
         raise SyntaxError('too many sign =')
+
+    result = round(result, 8)
+    #for case -0.0
+    if (result == 0):
+        return 0.0
+    return result
